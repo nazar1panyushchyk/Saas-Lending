967cc7279142411ca348e94a613a55ec26d7239e
nazar1panyushchyk <npanusik@gmail.com>
2026-07-01 20:53:54 +0300

    added large-logo animation & fixed details & finished project

--- src/js/script.js
+++ src/js/script.js
@@ -1,22 +1,37 @@
-let lightmode = localStorage.getItem(".light-mode");
+let lightmode = localStorage.getItem("light-mode");
 const themeSwitcher = document.querySelector(".theme-btn");
 const images = document.querySelectorAll(".theme-image");
 const menuBtn = document.querySelector(".menu-btn");
 const header = document.querySelector("header");
+const navLinks = document.querySelector(".nav-links");
+
+if ("scrollRestoration" in history) {
+  history.scrollRestoration = "manual";
+};
 
 const enableLightmode = () => {
   document.body.classList.add("light-mode");
   localStorage.setItem("light-mode", "active");
 };
 
-if (lightmode === "active") enableLightmode();
-
-updateThemeImages(lightmode === "active");
-
 const disableLightmode = () => {
   document.body.classList.remove("light-mode");
-  localStorage.setItem("light-mode", null);
+  localStorage.setItem("light-mode", "inactive");
 };
+
+function updateThemeImages(isLight) {
+  images.forEach((img) => {
+    img.src = isLight ? img.dataset.light : img.dataset.dark;
+  });
+}
+
+if (lightmode === "active") {
+  enableLightmode();
+  updateThemeImages(true);
+} else {
+  disableLightmode();
+  updateThemeImages(false);
+}
 
 themeSwitcher.addEventListener("click", () => {
   lightmode = localStorage.getItem("light-mode");
@@ -30,12 +45,19 @@ themeSwitcher.addEventListener("click", () => {
   }
 });
 
-function updateThemeImages(isLight) {
-  images.forEach((img) => {
-    img.src = isLight ? img.dataset.light : img.dataset.dark;
-  });
-}
+document.addEventListener("click", (e) => {
+  const isMenuOpen = header.classList.contains("menu-open");
 
-menuBtn.addEventListener("click", () => {
+  if (!isMenuOpen) return;
+
+  if (navLinks.contains(e.target)) return;
+
+
+  header.classList.remove("menu-open");
+
+}) 
+
+menuBtn.addEventListener("click", (e) => {
+  e.stopPropagation();
   header.classList.toggle("menu-open");
 });
